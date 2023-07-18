859ce891d5fa0f48eccbe55f582082be54d5710f
Manal Afzal <manal.afzal@ontariotechu.net>
2023-07-18 19:13:27 -0400

    Did some new work!

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: 'jit',
+    purge: [
+      './**/*.html'
+    ],
+    darkMode: false,
+    content: [],
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
